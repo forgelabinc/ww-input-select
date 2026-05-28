11959be22de5bcaf4b93cb0685f856ae141bb879
jptrinh <hello@jptrinh.com>
2026-05-28 15:56:16 +0200

    Prevent dropdown closure when it is already closed

--- src/wwElement_Select.vue
+++ src/wwElement_Select.vue
@@ -470,6 +470,7 @@ export default {
 
         function closeDropdown() {
             if (!shouldCloseDropdown.value) return;
+            if (!isOpen.value) return;
 
             resetSearch();
             resetFocus();
